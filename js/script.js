92e70aa6ee578cc9432860c20e20093c3f9400d3
Mihail Yaremenko <m50421@outlook.com>
2021-11-24 17:43:54 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let ev = document.addEventListener("DOMContentLoaded", () => {
+    let b;
+let c = null;
+let d = {data: null}
+// var client = new XMLHttpRequest();
+// client.open('GET', '/foo.txt');
+// client.onreadystatechange = function() {
+//     c = client.responseText;
+// }
+// client.send();
+
+fetch('foo.txt')
+  .then(response => response.text())
+  .then((data) => {
+      d.data = data
+    console.log(data)
+    console.log(d.data)
+    c = data;
+
+    console.log(c)
+  }).finally(() => {
+      
+    console.log(c);
+
+    console.log("c: " + c)
+  })
+
+// c.then((data) => {
+    // console.log(data);
+// })
+
+// alert("B: " + b);
+// alert("C: "+ c);
+})
